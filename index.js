e23561b001c535cd132b1d686ff753d70df5a4c5
taniashahida-dev <taniashahida993@gmail.com>
2026-06-19 09:57:40 +0600

    feat: initialize Express server and MongoDB connection

---
+++ index.js
@@ -0,0 +1,55 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+// const { createRemoteJWKSet, jwtVerify } = require("jose-cjs");
+const uri = process.env.MONGODB_URI;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+ 
+     const db = client.db("Fable-db");
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close()
+  }
+};
+
+run().catch(console.dir);
+
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
